452a03186ccc28b21da6cb8ffbe48e4a372572ff
IshtarGhost <reilbacher@go.pasadena.edu>
2026-03-02 11:50:56 -0800

    js added

---
+++ sketch_260302g.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  fill(0, 0, 255);
+  stroke(255, 255, 255);
+  noStroke();
+
+  // brown circle
+  fill(200, 154, 124);
+  ellipse(200, 200, 320, 320);
+
+  // yellow circle
+  fill(255, 255, 0);
+  ellipse(200, 200, 300, 300);
+
+  // blue eyes
+  fill(0, 71, 171);
+  ellipse(150, 150, 18, 33);
+  ellipse(250, 150, 18, 33);
+
+  // mouth in brown
+  fill(139, 69, 19);
+  ellipse(200, 250, 120, 100);
+
+  // teeth
+  fill(255, 255, 255);
+  ellipse(200, 232, 120, 90);
+
+  // remove teeth
+  fill(255, 255, 0);
+  ellipse(200, 220, 150, 100);
+}
